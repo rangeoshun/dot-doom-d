7ee35026dce0fbf45962ec9e866a5d432861fe97
Abel VARGA <avarga@whitepages.com>
2019-09-23 17:46:39 +0200

    Doom configs

---
+++ config.el
@@ -0,0 +1,46 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+;; Assign typescript-mode to .tsx files
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . tide-mode))
+
+;; Connfigure groovy-mode
+(add-hook 'groovy-mode-hook
+          (lambda ()
+            (setq groovy-indent-offset 2)
+            (setq indent-line-function 'indent-relative)))
+
+;; Magit magic
+;; Open PR with "v" in magit
+(defun endless/visit-pull-request-url ()
+  "Visit the current branch's PR on Github."
+  (interactive)
+  (browse-url
+   (format "https://github.com/%s/pull/new/%s"
+           (replace-regexp-in-string
+            "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
+            (magit-get "remote"
+                       (magit-get-push-remote)
+                       "url"))
+           (magit-get-current-branch))))
+(eval-after-load 'magit
+  '(define-key magit-mode-map "v"
+     #'endless/visit-pull-request-url))
+
+;; Fetch PRs
+(defun endless/add-PR-fetch ()
+  "If refs/pull is not defined on a GH repo, define it."
+  (let ((fetch-address
+         "+refs/pull/*/head:refs/pull/origin/*")
+        (magit-remotes
+         (magit-get-all "remote" "origin" "fetch")))
+    (unless (or (not magit-remotes)
+                (member fetch-address magit-remotes))
+      (when (string-match
+             "github" (magit-get "remote" "origin" "url"))
+        (magit-git-string
+         "config" "--add" "remote.origin.fetch"
+         fetch-address)))))
+
+(add-hook 'magit-mode-hook #'endless/add-PR-fetch)
