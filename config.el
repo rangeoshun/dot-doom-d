9e4c97ed65f04f25f1d8c0db01a66e75a9b67618
Abel Varga <avarga@hajtospincer.hu>
2019-10-02 09:22:14 +0200

    Configure js-mode

--- config.el
+++ config.el
@@ -8,42 +8,18 @@
 ;; Assign typescript-mode to .tsx files
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tide-mode))
 
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+(setq-default js-indent-level 2)
+
+(add-hook 'js2-mode-hook 'prettier-js-mode)
+(add-hook 'web-mode-hook 'prettier-js-mode)
+(add-hook 'json-mode-hook 'prettier-js-mode)
+(add-hook 'tide-mode-hook 'prettier-js-mode)
+(add-hook 'css-mode-hook 'prettier-js-mode)
+
 ;; Connfigure groovy-mode
 (add-hook 'groovy-mode-hook
           (lambda ()
             (setq groovy-indent-offset 2)
             (setq indent-line-function 'indent-relative)))
-
-;; Magit magic
-;; Open PR with "v" in magit
-(defun endless/visit-pull-request-url ()
-  "Visit the current branch's PR on Github."
-  (interactive)
-  (browse-url
-   (format "https://github.com/%s/pull/new/%s"
-           (replace-regexp-in-string
-            "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
-            (magit-get "remote"
-                       (magit-get-push-remote)
-                       "url"))
-           (magit-get-current-branch))))
-(eval-after-load 'magit
-  '(define-key magit-mode-map "v"
-     #'endless/visit-pull-request-url))
-
-;; Fetch PRs
-(defun endless/add-PR-fetch ()
-  "If refs/pull is not defined on a GH repo, define it."
-  (let ((fetch-address
-         "+refs/pull/*/head:refs/pull/origin/*")
-        (magit-remotes
-         (magit-get-all "remote" "origin" "fetch")))
-    (unless (or (not magit-remotes)
-                (member fetch-address magit-remotes))
-      (when (string-match
-             "github" (magit-get "remote" "origin" "url"))
-        (magit-git-string
-         "config" "--add" "remote.origin.fetch"
-         fetch-address)))))
-
-(add-hook 'magit-mode-hook #'endless/add-PR-fetch)
