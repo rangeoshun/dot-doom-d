ba909cbd0f113f34ffc8beb9fd90d26ece7fd57a
Abel VARGA <avarga@whitepages.com>
2019-10-22 11:10:13 +0200

    Do not let tide format before save

--- config.el
+++ config.el
@@ -31,18 +31,16 @@
   (tide-setup)
   (flycheck-mode +1)
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  ;; aligns annotation to the right hand side
+  (setq company-tooltip-align-annotations t)
   (eldoc-mode +1)
+  (tide-format-before-save )
   (tide-hl-identifier-mode +1)
   ;; company is an optional dependency. You have to
   ;; install it separately via package-install
   ;; `M-x package-install [ret] company`
   (company-mode +1))
 
-;; aligns annotation to the right hand side
-(setq company-tooltip-align-annotations t)
-
-;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 ;; Assign typescript-mode to .tsx files
